a329a4f32ac3de5c73e875b478d3061ec467e2fd
aryantisiregar <aryantisiregar2010@gmail.com>
2022-08-24 18:38:23 +0700

    add projek HelloWorld

---
+++ 3_Version Control With Git/praktikum/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+<<<<<<< HEAD
+        System.out.println("Hello, Nana!");
+=======
+        System.out.println("Hello, kamu!");
+>>>>>>> develop
+    }
+}
